bb806bb14bc5fe4eda90d1e8e846a46307d92ba4
laranjazda <carloseng.jr@gmail.com>
2021-09-16 23:09:28 -0300

    Initial commit

---
+++ src/main/java/com/HSOB/TestesSpringBoot/TestesSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.HSOB.TestesSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestesSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestesSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/HSOB/TestesSpringBoot/TestesSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HSOB.TestesSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestesSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
